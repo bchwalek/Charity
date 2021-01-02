8e67750e73620d3ba5fccae8989a4275b1f66a0d
bchwalek <bchwalek@interia.pl>
2021-01-02 23:25:40 +0100

    Entity Institution

---
+++ src/main/java/pl/coderslab/charity/institution/Institution.java
@@ -0,0 +1,41 @@
+package pl.coderslab.charity.institution;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Institution {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+    String decription;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+}
