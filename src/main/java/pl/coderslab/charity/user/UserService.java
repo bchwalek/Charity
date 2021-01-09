373a2a2edb9cd435d7bf78730d061b90c0bd7be1
bchwalek <bchwalek@interia.pl>
2021-01-09 03:05:52 +0100

    User entity, repository, service

---
+++ src/main/java/pl/coderslab/charity/user/UserService.java
@@ -0,0 +1,12 @@
+package pl.coderslab.charity.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@AllArgsConstructor
+@Transactional
+@Service
+public class UserService {
+}
